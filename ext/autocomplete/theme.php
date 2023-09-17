538fd89f75fe31cbffcd0cbaa00c474c15ec6449
Giraffaman <christopher.mallmann@outlook.com>
2023-09-17 23:14:03 +0200

    added theme-check + css import to autocomplete ext pr0booru theme css tweaks

--- ext/autocomplete/theme.php
+++ ext/autocomplete/theme.php
@@ -15,5 +15,14 @@ class AutoCompleteTheme extends Themelet
         $page->add_html_header("<script defer src='$base_href/ext/autocomplete/lib/tag-it.min.js' type='text/javascript'></script>");
         $page->add_html_header('<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css">');
         $page->add_html_header("<link rel='stylesheet' type='text/css' href='$base_href/ext/autocomplete/lib/jquery.tagit.css' />");
+        
+        // determine which theme is being used, then load that theme's css last for stuff like input button styles can overwrite jQuery if needed
+        $theme = get_theme();
+        if(!file_exists("themes/$theme")) {
+            // something went wrong, better not load anything
+            ;
+        } else {
+            $page->add_html_header("<link rel='stylesheet' type='text/css' href='$base_href/themes/$theme/style.css' />");
+        }
     }
 }
