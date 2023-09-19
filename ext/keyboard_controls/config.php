5ad3c967134587a89b9798cedaabc02dc9e689a3
Giraffaman <christopher.mallmann@outlook.com>
2023-09-19 23:46:16 +0200

    keyboard controls cont

--- ext/keyboard_controls/config.php
+++ ext/keyboard_controls/config.php
@@ -5,13 +5,6 @@ declare(strict_types=1);
 namespace Shimmie2;
 
 class keyBindings {
-    public const PREV_KEYS = array("a", "p");
-    public const NEXT_KEYS = array("d", "n");
-};
-
-return "
-<script type='text/javascript' language='javascript'>
-    var PREV_KEYS = <?php echo json_encode($PREV_KEYS); ?>;
-    var NEXT_KEYS = <?php echo json_encode($NEXT_KEYS); ?>;
-</script>
-"
+    public const PREV_KEYS = "previous_keys";
+    public const NEXT_KEYS = "next_keys";
+}
--- ext/keyboard_controls/main.php
+++ ext/keyboard_controls/main.php
@@ -5,3 +5,26 @@ declare(strict_types=1);
 namespace Shimmie2;
 
 require_once "config.php";
+
+class keyboardControls extends Extensino {
+    public function onInitExt(InitExtEvent $event) {
+        global $config;
+        $config->set_default_string(keyBindings::PREV_KEYS, array("a", "p"));
+        $config->set_default_string(keyBindings::NEXT_KEYS, array("d", "n"));
+    }
+
+    public function onPageRequest(onPageRequestEvent $event) {
+        global $page;
+        if($event->page_matched("post/view")) {
+            $pk = $config->get(keyBindings::PREV_KEYS);
+            $nk = $config->get(keyBindings::NEXT_KEYS);
+            return "
+<script type='text/javascript' language='javascript'>
+    var PREV_KEYS = <?php echo json_encode($pk); ?>;
+    var NEXT_KEYS = <?php echo json_encode($nk); ?>;
+</script>
+";
+        }
+    }
+}
+
