8a7d466ef859db94064a3de51f8db63016bd6408
Giraffaman <christopher.mallmann@outlook.com>
2023-09-20 00:50:08 +0200

    keyboard controls cont

--- ext/keyboard_controls/theme.php
+++ ext/keyboard_controls/theme.php
@@ -9,8 +9,8 @@ public function defineJsVars(Page $page) {
     $pk = $config->get_array(keyBindings::PREV_KEYS);
     $nk = $config->get_array(keyBindings::NEXT_KEYS);
     $page->add_html_header("
-<script type='text/javascript' language='javascript'>
+<script type='text/javascript'>
     var PREV_KEYS = ".json_encode($pk).";
     var NEXT_KEYS = ".json_encode($nk).";
-</script>")
+</script>");
 }
