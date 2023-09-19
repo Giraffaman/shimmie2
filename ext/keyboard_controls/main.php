8ce371eb622e7da47a86826b11e2c20900d2b1f6
Giraffaman <christopher.mallmann@outlook.com>
2023-09-19 23:58:16 +0200

    keyboard controls cont

--- ext/keyboard_controls/main.php
+++ ext/keyboard_controls/main.php
@@ -9,19 +9,19 @@ require_once "config.php";
 class keyboardControls extends Extensino {
     public function onInitExt(InitExtEvent $event) {
         global $config;
-        $config->set_default_string(keyBindings::PREV_KEYS, array("a", "p"));
-        $config->set_default_string(keyBindings::NEXT_KEYS, array("d", "n"));
+        $config->set_default_array(keyBindings::PREV_KEYS, array("a", "p"));
+        $config->set_default_array(keyBindings::NEXT_KEYS, array("d", "n"));
     }
 
     public function onPageRequest(onPageRequestEvent $event) {
         global $page;
         if($event->page_matched("post/view")) {
-            $pk = $config->get(keyBindings::PREV_KEYS);
-            $nk = $config->get(keyBindings::NEXT_KEYS);
+            
+            
             return "
 <script type='text/javascript' language='javascript'>
-    var PREV_KEYS = <?php echo json_encode($pk); ?>;
-    var NEXT_KEYS = <?php echo json_encode($nk); ?>;
+    var PREV_KEYS = <?php echo json_encode($config->get_array(keyBindings::PREV_KEYS);); ?>;
+    var NEXT_KEYS = <?php echo json_encode($config->get_array(keyBindings::NEXT_KEYS);); ?>;
 </script>
 ";
         }
