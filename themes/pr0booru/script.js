17ab02ebbf951cf93506a79d6be85fb48db42f3e
Giraffaman <christopher.mallmann@outlook.com>
2023-09-21 00:07:51 +0200

    add keyboard controls (directly via pr0booru theme)

--- themes/pr0booru/script.js
+++ themes/pr0booru/script.js
@@ -1,14 +1,44 @@
 document.addEventListener('keydown', (e) => {
+    // for testing if this is active
     alert("running");
+
+    var PREV_KEYS = ("a","p");
+    var NEXT_KEYS = ("d","n");
+    var PRPG_KEYS = ("q");
+    var NXPG_KEYS = ("e");
+    var FAV_KEYS = ("f");
+    //var PLAY_KEYS = ("???")
+
     switch(e.key) {
         case $PREV_KEYS.includes(e.key):
-            $target = document.getElementById("prevlink").getAttribute("href");
-            window.location.href=$target;
+            if(window.location.pathname.match("/post/view/")) {
+                $target = document.getElementById("prevlink").getAttribute("href");
+                window.location.href=$target;
+            } else {
+                break;
+            }
             break;
         case $NEXT_KEYS.includes(e.key):
-            $target = document.getElementById("nextlink").getAttribute("href");
-            window.location.href=$target;
+            if(window.location.pathname.match("/post/view/")) {
+                $target = document.getElementById("nextlink").getAttribute("href");
+                window.location.href=$target;
+            } else {
+                break;
+            }
             break;
+        case "f":
+            // grab "Favorite"/"Un-Favorite"-button and click it
+            $fb = document.querySelector('\
+                section#Post_Controlsleft > div.blockbody > form[action="/change_favorite"] > input[value="Favorite"],\
+                section#Post_Controlsleft > div.blockbody > form[action="/change_favorite"] > input[value="Un-Favorite"]\
+            ');
+            if($fb) {
+                $fb.click();
+                break;
+            } else {
+                break;
+            }
+$fb.click();
         case "q":
             /*
                 do something to:
